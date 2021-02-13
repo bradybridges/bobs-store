26a357cc86ec071ed5ae7c4995fcefd2f7e0f6dc
Brady Bridges <bradyjbridges@gmail.com>
2021-02-13 15:12:18 -0700

    Add fetch to get products by search query and page number

--- src/api/products.js
+++ src/api/products.js
@@ -1,1 +1,14 @@
 // API calls to get product data
+
+export const searchProducts = async (search, page = 1) => {
+  const endpoint = `http://api.searchspring.net/api/search/search.json?siteId=scmq7n&q=${search}&resultsFormat=native&page=${page}`
+
+  const response = await fetch(endpoint)
+
+  if (!response.ok) {
+    throw new Error(response.status)
+  }
+
+  const products = await response.json()
+  return products
+}
