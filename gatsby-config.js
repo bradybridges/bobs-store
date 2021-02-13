59aa32bf375db3faa6696666d325cf85f1346aa2
Brady Bridges <bradyjbridges@gmail.com>
2021-02-13 10:15:10 -0700

    Update site metadata, configure SCSS support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bobs Site`,
+    description: `We provide a wide range of high-end clothing at affordable prices.`,
+    author: `@bradybridges`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
