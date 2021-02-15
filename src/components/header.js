ba5a2070ba770777df8b2531fcb027b620cfe35b
Brady Bridges <bradyjbridges@gmail.com>
2021-02-15 09:09:54 -0700

    Move header styles from component to stylesheet

--- src/components/header.js
+++ src/components/header.js
@@ -5,23 +5,9 @@ import "./header.scss"
 
 const Header = ({ siteTitle }) => (
   <header className="bobs-header">
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+    <div className="bob-header__wrapper">
+      <h1 className="bob-header__title">
+        <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
   </header>
